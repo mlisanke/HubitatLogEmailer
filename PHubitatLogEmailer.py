f359178d6128060c2fc4323cf9bcf801183f21dc
Mike Lisanke <MikeLisanke@gmail.com>
2022-05-31 04:46:29 -0400

    Add continous temperature and battery logging every 5m to the email stats!

--- PHubitatLogEmailer.py
+++ PHubitatLogEmailer.py
@@ -12,6 +12,9 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 import os.path
 import pickle
+import platform
+import sys
+import string
 
 def PickleDump(ip,cookies,username,password):
     with open(".hubitat-env","wb") as outfile:
@@ -27,6 +30,25 @@ def PickleLoad():
             cookies = pickle.load(infile)
             username = pickle.load(infile)
             password = pickle.load(infile)
+    else:
+        ip = input("Hubitat IP address:")
+        hkey = input("Hubitat Key Name:")
+        hvalue = input("Hubitat Key Value:")
+        cookies = {hkey:hvalue}
+        username = input("Gmail userame:")
+        password = input("Gmail password:")
+    return (ip,cookies,username,password)
+
+def PickleLoadNewKey():
+    if os.path.isfile(".hubitat-env"):
+        with open(".hubitat-env","rb") as infile:
+            ip = pickle.load(infile)
+            cookies = pickle.load(infile)
+            username = pickle.load(infile)
+            password = pickle.load(infile)
+            hkey = list(cookies)[0]
+            cookies[hkey] = input("new Hubitat Key Value:")
+            PickleDump(ip,cookies,username,password)
     else:
         ip = input("Hubitat IP address:")
         hkey = input("Hubitat Key Name:")
@@ -65,6 +87,17 @@ def send_mail(send_from, send_to, subject, text, files=None, send_cc=None, send_
     server.login(user,passwd)
     server.sendmail(send_from, send_to, msg.as_string())
     server.quit()
+    
+alut = {}    
+def chklutime(name,time,mindiff): 
+    if name not in alut:
+        alut[name] = time
+        return True
+    ludiff = time-alut[name]
+    if ludiff.total_seconds() > mindiff:
+        alut[name] = time
+        return True
+    return False
 
 def main():
     print("Hubitat Log Emailer")
@@ -78,7 +111,7 @@ def main():
 
     print(r.status_code)
     print(r.headers)
- #   print(r.content)
+#   print(r.content)
 
     t = datetime.now()
 
@@ -91,8 +124,34 @@ def main():
 
     print(pt,t)
 
+    devs = { 'Weather 1':'Back side of house',
+'Contact Sensor 1':'Family room door contact',
+'Contact Sensor 2':'Living room door contact',
+'Garage Door Tilt Sensor':'Garage Door Tilt Sensor',
+'IR Motion 1':'Family room near Inside Door motion',
+'IR Motion 2':'Garage door (defunked) motion',
+'IR Motion 3':'Living room near Front door motion',
+'IR Motion 4':'Living room near North window motion',
+'IR Motion 5':'Kitchen near cupboard (defunked) motion',
+'IR Motion 6':'Family room at Stairs motion',
+'IR Motion 7':'Garage door motion',
+'IR Motion 8':'Kitchen near Cupboard motion',
+'Key Fob 1':'backup fob',
+'Key Fob 2':'primary fob',
+'Outlet 1':'Living room near North window outlet',
+'Siren 1':'Bookshelf near stairs siren',
+'Siren 2':'Kitchen above cupboard siren',
+'Siren 3':'Living room above Grandfather clock siren',
+'Siren 4':'Office bookshelf near hall siren',
+'Siren 5':'Garage siren',
+'Siren 6':'Kitchen siren',
+'Smoke/CO Detector 1':'Family room shelf near recliner smoke/CO',
+'Smoke/CO Detector 2':'Upstairs hall near primary smoke alarm smoke/CO',
+'Motion Light Temp Humidity 1':'Front porch motion',
+'Motion Light Temp Humidity 2':'Back porch motion' }
+
     elist = []
-    eapp1 = []
+    enote = []
     eintrusion = None
     ct = datetime.now()
     offset = datetime.fromtimestamp(ct.timestamp()) - datetime.utcfromtimestamp(ct.timestamp())
@@ -101,25 +160,54 @@ def main():
         edate, etime = edt.split()
         ehour,emin,esec = etime.split(":")
         etype,eid,ename,etext = content.split('|')
+        edt = edt.lstrip("\\u0000")
         ut = datetime.strptime(edt+"000","%Y-%m-%d %H:%M:%S.%f")
         lt = ut+offset
         if lt > pt:
             date = lt.strftime("%Y-%m-%d %H:%M:%S.%f")
             elist.append([date,level,etype,eid,ename,etext])
             if etype == 'app' and eid == '1':
-                eapp1.append(lt.strftime("%m/%d %H:%M ")+etext)
+                enote.append(lt.strftime("%m/%d %H:%M ")+etext)
                 if "Intrusion" in etext:
                     eintrusion = etext
-#    print(apps,elist)
+            elif etype == 'dev':
+                dname = devs.get(ename,'Device not found')
+                if etext.find('is open') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+dname+' is open ('+etext+')')
+                if etext.find('was opened') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+dname+' is open ('+etext+')')
+                elif etext.find('is closed') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+dname+' is closed ('+etext+')')
+                elif etext.find('was closed') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+dname+' is closed ('+etext+')')
+                elif etext.find('is active') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+dname+' is active ('+etext+')')
+                elif etext.find('is inactive') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+dname+' is inactive ('+etext+')')
+                elif ename.find('Key Fob') != -1:
+                    enote.append(lt.strftime("%m/%d %H:%M ")+'('+etext+')')
+                elif etext.find('temperature is') != -1:
+                    if chklutime(ename,lt,300):
+                        enote.append(lt.strftime("%m/%d %H:%M ")+etext.replace("\\u00b0"," deg."))
+                elif etext.find('battery is') != -1:
+                    if chklutime(ename,lt,300):
+                        enote.append(lt.strftime("%m/%d %H:%M ")+etext)    
+#    print(note,elist)
 
     mtext = ""
+    lline = ""
     with open("hubitat.txt","w") as outfile:
-        for aline in eapp1:
-            print(aline)
-            outfile.write(aline+"\n")
-            mtext += aline+"\n"
+        for aline in enote:
+            if aline != lline:
+                print(aline)
+                outfile.write(aline+"\n")
+                mtext += aline+"\n"
+                lline = aline
+        if platform.system() == "Linux":
+            with open('/proc/uptime', 'r') as f:
+                mtext += "uptime: "+f.readline().split()[0]+"\n"
         outfile.write("\n")
-        mtext += "\nAttached Hubitat Past Log File\n"
+        mtext += "\n- Attached Hubitat Past Log File\n"
         for e in elist:
             date,level,etype,eid,ename,etext = e
             etext=etext.replace("\\u00b0"," deg.")
@@ -145,4 +233,7 @@ def main():
     send_mail(fromaddr,[toaddr],subject,mtext,files=["hubitat.txt"],send_cc=cc,send_bcc=bcc,user=username,passwd=password)
 
 if __name__=="__main__":
+        if len(sys.argv) > 1:
+            if "newkey" == sys.argv[1]:
+                PickleLoadNewKey()
         main()
